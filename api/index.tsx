097b1cd08bef2baf10f8f8485f4c05b3efc8fafa
goftok <skills.lancets0n@icloud.com>
2024-06-23 21:28:10 +0200

    adding redirection

--- api/index.tsx
+++ api/index.tsx
@@ -3,6 +3,8 @@ import { devtools } from 'frog/dev'
 import { serveStatic } from 'frog/serve-static'
 // import { neynar } from 'frog/hubs'
 import { handle } from 'frog/vercel'
+import { generateImageComponent } from './utils.js'
+
 
 // Uncomment to use Edge Runtime.
 // export const config = {
@@ -12,9 +14,11 @@ import { handle } from 'frog/vercel'
 export const app = new Frog({
   assetsPath: '/',
   basePath: '/api',
+  browserLocation: '/:path',
   // Supply a Hub to enable frame verification.
   // hub: neynar({ apiKey: 'NEYNAR_FROG_FM' })
 })
+
 
 interface TokenStore {
   [key: string]: number;
@@ -22,59 +26,73 @@ interface TokenStore {
 
 const tokenStore: TokenStore = {};
 
+
 app.frame('/', (c) => {
-  const { buttonValue, status } = c;
-  const initialTokens = 10;
   const userId = 'someUserId';
+  tokenStore[userId] = tokenStore[userId] ?? 10;
+  const hello_message = `Welcome to the Plum thimbles game!\nYour current balance is ${tokenStore[userId]} $PLUM`;
+  console.log(tokenStore[userId])
 
-  tokenStore[userId] = tokenStore[userId] ?? initialTokens;
-
-  const winningButton = Math.floor(Math.random() * 3) + 1;
-
-  let resultMessage = 'Choose a number: 1, 2, or 3. If you win, you get 2 tokens. If you lose, you lose 1 token. You have 10 tokens to start.';
-  if (status === 'response') {
-    if (buttonValue?.toString() == winningButton.toString()) {
-      tokenStore[userId] += 2;
-      resultMessage = `You win! Your tokens: ${tokenStore[userId]}`;
-    } else {
-      tokenStore[userId] -= 1;
-      resultMessage = `You lose. Your tokens: ${tokenStore[userId]}`;
-    }
-  }
   return c.res({
-    image: (
-      <div
-        style={{
-          alignItems: 'center',
-          background: 'linear-gradient(to right, #432889, #17101F)',
-          display: 'flex',
-          flexDirection: 'column',
-          height: '100%',
-          justifyContent: 'center',
-          textAlign: 'center',
-          width: '100%',
-        }}
-      >
-        <div
-          style={{
-            color: 'white',
-            fontSize: 60,
-            letterSpacing: '-0.025em',
-            lineHeight: 1.4,
-            marginTop: 30,
-            padding: '0 120px',
-            whiteSpace: 'pre-wrap',
-          }}
-        >
-          {resultMessage}
-        </div>
-      </div>
-    ),
+    action: '/mixing',
+    image: generateImageComponent(hello_message),
+    intents: [
+      <Button value='start' >Play</Button>,
+      <Button.Redirect location="/docs">Rules</Button.Redirect>
+    ]
+  });
+});
+
+
+app.frame('/mixing', (c) => {
+  const mixing_message = "/mixing gif with thimbles/";
+
+  return c.res({
+    action: '/choose-thimble',
+    image: generateImageComponent(mixing_message),
+    intents: [
+      <Button value='stop'>Press to stop mixing thimbles</Button>,
+    ]
+  });
+});
+
+
+app.frame('/choose-thimble', (c) => {
+  let resultMessage = '/image of the thimbles/\nChoose a number: 1, 2, or 3. If you win, you get 2 tokens. If you lose, you lose 1 token.';
+  return c.res({
+    action: '/result-thimble',
+    image: generateImageComponent(resultMessage),
     intents: [
       <Button value="1">1</Button>,
       <Button value="2">2</Button>,
       <Button value="3">3</Button>,
-      status === 'response' && <Button.Reset>Reset</Button.Reset>,
+    ],
+  });
+});
+
+
+app.frame('/result-thimble', (c) => {
+  const { buttonValue } = c;
+  const userId = 'someUserId';
+
+  const winningButton = Math.floor(Math.random() * 3) + 1;
+
+  let resultMessage = '/opening one thimble/\n'
+
+  if (buttonValue?.toString() == winningButton.toString()) {
+    tokenStore[userId] += 2;
+    resultMessage += `You win! Your tokens: ${tokenStore[userId]}`;
+  } else {
+    tokenStore[userId] -= 1;
+    resultMessage += `You lose. Your tokens: ${tokenStore[userId]}`;
+  }
+
+  return c.res({
+    action: '/choose-thimble',
+    image: generateImageComponent(resultMessage),
+    intents: [
+      <Button value="play-again"> Play Again</Button>,
+      <Button.Reset>Return</Button.Reset>
     ],
   });
 });
---
+++ api/utils.tsx
@@ -0,0 +1,30 @@
+export const generateImageComponent = (text: string) => {
+    return (
+        <div
+            style={{
+                alignItems: 'center',
+                background: 'linear-gradient(to right, #432889, #17101F)',
+                display: 'flex',
+                flexDirection: 'column',
+                height: '100%',
+                justifyContent: 'center',
+                textAlign: 'center',
+                width: '100%',
+            }}
+        >
+            <div
+                style={{
+                    color: 'white',
+                    fontSize: 60,
+                    letterSpacing: '-0.025em',
+                    lineHeight: 1.4,
+                    marginTop: 30,
+                    padding: '0 120px',
+                    whiteSpace: 'pre-wrap',
+                }}
+            >
+                {text}
+            </div>
+        </div>
+    );
+};
