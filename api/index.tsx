e689ee0d016e0976cb6e589151482cc7de1b9dda
goftok <skills.lancets0n@icloud.com>
2024-06-23 21:34:54 +0200

    moving generateImageComponent from utils

--- api/index.tsx
+++ api/index.tsx
@@ -3,7 +3,6 @@ import { devtools } from 'frog/dev'
 import { serveStatic } from 'frog/serve-static'
 // import { neynar } from 'frog/hubs'
 import { handle } from 'frog/vercel'
-import { generateImageComponent } from './utils.js'
 
 
 // Uncomment to use Edge Runtime.
@@ -26,6 +25,36 @@ interface TokenStore {
 
 const tokenStore: TokenStore = {};
 
+export const generateImageComponent = (text: string) => {
+  return (
+    <div
+      style={{
+        alignItems: 'center',
+        background: 'linear-gradient(to right, #432889, #17101F)',
+        display: 'flex',
+        flexDirection: 'column',
+        height: '100%',
+        justifyContent: 'center',
+        textAlign: 'center',
+        width: '100%',
+      }}
+    >
+      <div
+        style={{
+          color: 'white',
+          fontSize: 60,
+          letterSpacing: '-0.025em',
+          lineHeight: 1.4,
+          marginTop: 30,
+          padding: '0 120px',
+          whiteSpace: 'pre-wrap',
+        }}
+      >
+        {text}
+      </div>
+    </div>
+  );
+};
 
 app.frame('/', (c) => {
   const userId = 'someUserId';
